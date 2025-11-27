0ad18b422b6031b00627ed93d15fe6996e9290de
Julius257 <153816414+Julius257@users.noreply.github.com>
2025-11-27 09:43:09 +0100

    fix: imports & Navigationsitems

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,9 @@
 import { Link, Outlet } from "react-router";
 import {
   NavigationMenu,
+  NavigationMenuItem,
   NavigationMenuLink,
+  NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 
@@ -11,13 +13,19 @@ function Layout() {
     <div className="min-h-screen flex flex-col">
       <header className="bg-amber-300 flex gap-3 p-3 place-content-center">
 
-        <NavigationMenu className="flex gap-3">
-          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link to="/">Home</Link>
-          </NavigationMenuLink>
-          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link to="/login">Login</Link>
-          </NavigationMenuLink>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                <Link to="/">Home</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                <Link to="/login">Login</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
         </NavigationMenu>
 
       </header>

b9712b0184eef074c44f9a27cbeb089513b75b68
Julius <153816414+Julius257@users.noreply.github.com>
2025-11-27 11:57:11 +0100

    feat: Router & Basic Layout hinzugefügt (#7)
    * feat: Router & Basic Layout hinzugefügt
    
    * fix: imports & Navigationsitems
    
    * node shit
    
    ---------
    
    Co-authored-by: Jui <jui.scro@gmail.com>

---
+++ src/components/Layout.tsx
@@ -0,0 +1,40 @@
+import { Link, Outlet } from "react-router";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
+
+function Layout() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-amber-300 flex gap-3 p-3 place-content-center">
+
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                <Link to="/">Home</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                <Link to="/login">Login</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+
+      </header>
+
+      <main className="flex-1">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
